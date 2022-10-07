e5ed53b10b757b004ef53436228666b97f3b8c8b
hrishabh <hrishabhyadav96@gmail.com>
2022-10-08 00:37:30 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 
 
-const App = () => {
+export default () => {
     const [fullName, setFullName] = useState( {
       fname : '', 
       email : '',
@@ -81,4 +81,4 @@ const App = () => {
     );
       
 };
-export default App;
+
